6ed5cd91f896164694849220e29f3b8700d9462c
bear08 <957011893@qq.com>
2023-07-24 17:34:28 +0800

    wifi connect modify

--- micropython/wifi/connect.py
+++ micropython/wifi/connect.py
@@ -1,7 +1,21 @@
 import network
-from machine import Timer
-from machine import Pin
 import time
+
+def statusName(status):
+    if network.STAT_IDLE ==status:
+        return "idle"
+    elif network.STAT_CONNECTING ==status:
+        return "connecting"
+    elif network.STAT_WRONG_PASSWORD  ==status:
+        return "wrong password"
+    elif network.STAT_NO_AP_FOUND  ==status:
+        return "no ap found"
+    elif network.STAT_CONNECT_FAIL  ==status:
+        return "connect fail"
+    elif network.STAT_GOT_IP  ==status:
+        return "got ip"
+    else:
+        return "unknown status"
 
 wlan=network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -11,14 +25,15 @@ for i in range(5):
     if wlan.isconnected():
         break
     else:
-        print("connecting...")
+        print("current status:",statusName(wlan.status()))
         time.sleep(1)
 
 if wlan.isconnected():
     print(wlan.ifconfig())
-    wlan.disconnect()
 else:
     print("connect failed")
+    print(statusName(wlan.status()))
+
 
 
 
