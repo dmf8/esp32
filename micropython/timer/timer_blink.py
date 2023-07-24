904de150c02a8832463119dc34b62c4075a4bb24
bear08 <957011893@qq.com>
2023-07-24 09:55:33 +0800

    add new timer method

--- micropython/timer/timer_blink.py
+++ micropython/timer/timer_blink.py
@@ -1,3 +1,16 @@
 from machine import Timer
 from machine import Pin
 
+t0=Timer(0)
+led = Pin(2, Pin.OUT)
+
+def pin_op(t):
+    led.value(1-led.value())
+
+def blink(half_period):
+	t0.init(mode=Timer.PERIODIC,period=half_period,callback=pin_op)
+
+def stop():
+    t0.deinit()
+    
+
---
+++ micropython/uart/blink_once.py
@@ -0,0 +1,9 @@
+from machine import Timer
+from machine import Pin
+
+t0=Timer(0)
+led = Pin(2, Pin.OUT)
+
+def blinkOnce(on_time):
+    led.on()
+    t0.init(mode=Timer.ONE_SHOT,period=on_time,callback=lambda t: led.off())
